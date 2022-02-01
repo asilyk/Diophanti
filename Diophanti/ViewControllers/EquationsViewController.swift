ed6b492e7741ce2f2a2c75960c9a20dddf61a3a7
fabet <asilyk112@gmail.com>
2022-02-01 11:00:56 +0300

    Create project hierarchy

---
+++ Diophanti/ViewControllers/EquationsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EquationsViewController.swift
+//  Diophanti
+//
+//  Created by Alexander on 01.02.2022.
+//
+
+import UIKit
+
+class EquationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
