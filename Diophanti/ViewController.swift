2a2d2505ef708ef795f95ee4e638a2b13ec70b2b
fabet <asilyk112@gmail.com>
2022-02-01 10:44:48 +0300

    Initial Commit

---
+++ Diophanti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diophanti
+//
+//  Created by Alexander on 01.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
