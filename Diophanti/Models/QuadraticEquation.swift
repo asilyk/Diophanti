8446e39356fa94c41c0a2c6dd8bbeeee4e777217
fabet <asilyk112@gmail.com>
2022-02-02 12:54:04 +0300

    Code refactoring

--- Diophanti/Models/QuadraticEquation.swift
+++ Diophanti/Models/QuadraticEquation.swift
@@ -12,7 +12,7 @@ struct QuadraticEquation {
     let secondCoefficient: Int
     let freeMember: Int
 
-    var discriminant : Int {
+    var discriminant: Int {
         Int(pow(Double(secondCoefficient), 2)) - 4 * firstCoefficient * freeMember
     }
 
@@ -22,57 +22,58 @@ struct QuadraticEquation {
         self.freeMember = freeMember
     }
 
-    func solve() -> [String] {
-        var solution: [String] = []
+    func solve() -> (String, String) {
+        var solution: String
+        var root = ""
 
-        if (firstCoefficient == 0) {
-            solution.append("Error! This equation is not quadratic.")
-            return solution
+        if firstCoefficient == 0 {
+            solution = "Error! This equation is not quadratic."
+            return (solution, solution)
         }
 
-        solution.append("""
-            To find the roots of a quadratic equation, it is necessary to find the discriminant according to the formula D = b² - 4ac where b = \(secondCoefficient), a = \(firstCoefficient), c = \(freeMember)
-            """)
-        solution.append("D = \(secondCoefficient)² - 4 * \(firstCoefficient) * \(freeMember) = \(discriminant)")
+        solution = """
+        To find the roots of a quadratic equation, it is necessary to find the discriminant according to the formula D = b² - 4ac where b = \(secondCoefficient), a = \(firstCoefficient), c = \(freeMember)
+        D = \(secondCoefficient)² - 4 * \(firstCoefficient) * \(freeMember) = \(discriminant)\n
+        """
 
         if discriminant < 0 {
-            solution.append("This quadratic equation has no roots because D < 0")
-            return solution
+            root = "This quadratic equation has no roots because D < 0"
+            solution += root
+            return (root, solution)
         }
 
         if discriminant == 0 {
-            solution.append("This quadratic equation has only one root because D = 0")
-            solution.append("We can find this root using the formula x = -b / 2a where b = \(secondCoefficient), a = \(firstCoefficient)")
-
-            let root = String(format: "%.2f", -secondCoefficient / (2 * firstCoefficient))
-
-            solution.append("x = -\(secondCoefficient) / (2 * \(firstCoefficient))")
-
-            solution.append("The root is")
-            solution.append("x ≈ \(root)")
+            root = String(format: "%.2f", Double(-secondCoefficient) / Double(2 * firstCoefficient))
+            solution += """
+            This quadratic equation has only one root because D = 0
+            We can find this root using the formula x = -b / 2a where b = \(secondCoefficient), a = \(firstCoefficient)
+            x = -\(secondCoefficient) / (2 * \(firstCoefficient))
+            The root is
+            x ≈ \(root)
+            """
         }
 
         if discriminant > 0 {
-            solution.append("This quadratic equation has two roots because D > 0")
-            solution.append("We can find this roots using the formula x = (-b ± √D) / 2a where b = \(secondCoefficient), D = \(discriminant), a = \(firstCoefficient)")
-
             let firstRoot = String(format: "%.2f", (-Double(secondCoefficient) - sqrt(Double(discriminant))) / (2 * Double(firstCoefficient)))
             let secondRoot = String(format: "%.2f", (-Double(secondCoefficient) + sqrt(Double(discriminant))) / (2 * Double(firstCoefficient)))
 
             let rootOfDiscriminant = String(format: "%.2f", sqrt(Double(discriminant)))
 
-            solution.append("x₁ = (\(-secondCoefficient) - √\(discriminant)) / (2 * \(firstCoefficient))")
-            solution.append("x₁ = (\(-secondCoefficient) - \(rootOfDiscriminant) / \(2 * firstCoefficient)")
-            solution.append("x₁ = \(String(format: "%.2f", Double(-secondCoefficient) - sqrt(Double(discriminant)))) / \(2 * firstCoefficient) = \(firstRoot)")
-            
-            solution.append("x₂ = (\(-secondCoefficient) + √\(discriminant)) / (2 * \(firstCoefficient))")
-            solution.append("x₂ = (\(-secondCoefficient) + \(rootOfDiscriminant)) / \(2 * firstCoefficient)")
-            solution.append("x₂ = \(String(format: "%.2f", Double(-secondCoefficient) + sqrt(Double(discriminant)))) / \(2 * firstCoefficient) = \(secondRoot)")
-
-            solution.append("The roots are")
-            solution.append("x₁ ≈ \(firstRoot)\nx₂ ≈ \(secondRoot)")
+            root = "x₁ ≈ \(firstRoot)\nx₂ ≈ \(secondRoot)"
+            solution += """
+            This quadratic equation has two roots because D > 0
+            We can find this roots using the formula x = (-b ± √D) / 2a where b = \(secondCoefficient), D = \(discriminant), a = \(firstCoefficient)
+            x₁ = (\(-secondCoefficient) - √\(discriminant)) / (2 * \(firstCoefficient))
+            x₁ = (\(-secondCoefficient) - \(rootOfDiscriminant) / \(2 * firstCoefficient)
+            x₁ = \(String(format: "%.2f", Double(-secondCoefficient) - sqrt(Double(discriminant)))) / \(2 * firstCoefficient) = \(firstRoot)
+            x₂ = (\(-secondCoefficient) + √\(discriminant)) / (2 * \(firstCoefficient))
+            x₂ = (\(-secondCoefficient) + \(rootOfDiscriminant)) / \(2 * firstCoefficient)
+            x₂ = \(String(format: "%.2f", Double(-secondCoefficient) + sqrt(Double(discriminant)))) / \(2 * firstCoefficient) = \(secondRoot)
+            The roots are
+            \(root)
+            """
         }
 
-        return solution
+        return (root, solution)
     }
 }
