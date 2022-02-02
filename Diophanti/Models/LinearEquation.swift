d9af28d45bc06b164e292d38b8d4b3785129ef76
fabet <asilyk112@gmail.com>
2022-02-02 12:32:15 +0300

    Fix bugs in text of detailed solution

--- Diophanti/Models/LinearEquation.swift
+++ Diophanti/Models/LinearEquation.swift
@@ -21,19 +21,19 @@ struct LinearEquation {
         let root: String
 
         if coefficient == 0 {
-            solution = "Error! This equation is not linear.\n"
+            solution = "Error! This equation is not linear."
             return (solution, solution)
         }
 
         root = "x ≈ \(Double(-firstFreeMember + secondFreeMember) / Double(coefficient))"
         solution = """
-            To solve a linear equation, we transfer all free members to the right side of the equation\n"
-            \(coefficient)x = \(-firstFreeMember) + \(secondFreeMember)
-            \(coefficient)x = \(-firstFreeMember + secondFreeMember)
-            Now we divide the right side of the equation by the coefficient before x
-            x = \(-firstFreeMember + secondFreeMember) / \(coefficient)
-            The root is\n"
-            x ≈ \(root))
+        To solve a linear equation, we transfer all free members to the right side of the equation
+        \(coefficient)x = \(-firstFreeMember) + \(secondFreeMember)
+        \(coefficient)x = \(-firstFreeMember + secondFreeMember)
+        Now we divide the right side of the equation by the coefficient before x
+        x = \(-firstFreeMember + secondFreeMember) / \(coefficient)
+        The root is
+        \(root)
         """
 
         return (root, solution)
