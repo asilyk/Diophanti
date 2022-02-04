a12d30b4ad7ec428b14f88cf6baba32d6ca5eee7
fabet <asilyk112@gmail.com>
2022-02-04 18:35:27 +0300

    Revert "Delete extra project directory"
    This reverts commit 04f2b4de7b182b8d37e5ff9e7557bb801c6c4322.

---
+++ Diophanti/Services/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  Diophanti
+//
+//  Created by Alexander on 01.02.2022.
+//
+
+import Foundation
---
+++ Diophanti/ViewControllers/AboutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diophanti
+//
+//  Created by Alexander on 01.02.2022.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
